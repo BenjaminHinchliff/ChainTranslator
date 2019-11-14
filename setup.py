b6ed55cd0d872016ba326879ae46c4cfd5c617c2
SuniTheFish <benjamin.hinchliff@gmail.com>
2019-11-13 19:22:43 -0800

    added pypi config

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from distutils.core import setup
 
 setup(
-    name="ChainTranslate",
-    packages=["ChainTranslate"],
+    name="ChainTranslator",
+    packages=["ChainTranslator"],
     version="1.0.0",
     license="MIT",
     description="a package to mess up text via google translate",
     author="Benjamin Hinchliff",
     url="https://github.com/SuniTheFish/ChainTranslator",
-    
+    download_url="https://github.com/SuniTheFish/ChainTranslator/archive/1.0.0.tar.gz",
+    keywords=["google", "translator", "useless", "stupid", "random", "chain", "translator", "ChainTranslator"],
+    install_requires=[
+        'googletrans'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Text Processing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7'
+    ]
 )
