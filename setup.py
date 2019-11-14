f5653da21c5dbc2e2b33d006f0c1321300ec1186
SuniTheFish <benjamin.hinchliff@gmail.com>
2019-11-13 19:13:15 -0800

    added python implmentation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="ChainTranslate",
+    packages=["ChainTranslate"],
+    version="1.0.0",
+    license="MIT",
+    description="a package to mess up text via google translate",
+    author="Benjamin Hinchliff",
+    url="https://github.com/SuniTheFish/ChainTranslator",
+    
+)
