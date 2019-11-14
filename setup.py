9fe400074344684bf02f277fb3a37b642d1e64fb
SuniTheFish <benjamin.hinchliff@gmail.com>
2019-11-13 19:53:30 -0800

    stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
+import pathlib
 from distutils.core import setup
 
 setup(
     name="ChainTranslator",
-    packages=["ChainTranslator"],
-    version="1.0.0",
+    packages=[],
+    version="1.0.2",
     license="MIT",
     description="a package to mess up text via google translate",
     author="Benjamin Hinchliff",
+    author_email="benjamin.hinchliff@gmail.com",
     url="https://github.com/SuniTheFish/ChainTranslator",
-    download_url="https://github.com/SuniTheFish/ChainTranslator/archive/1.0.0.tar.gz",
     keywords=["google", "translator", "useless", "stupid", "random", "chain", "translator", "ChainTranslator"],
     install_requires=[
         'googletrans'
@@ -18,6 +19,11 @@ setup(
         'Intended Audience :: End Users/Desktop',
         'Topic :: Text Processing',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7'
-    ]
+        'Programming Language :: Python :: 3'
+    ],
+    entry_points={
+        "console_scripts": [
+            "chaintranslator=ChainTranslator.__main__:main"
+        ]
+    }
 )
